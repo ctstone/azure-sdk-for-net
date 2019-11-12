6305a1b3d848004508d8af34b4eb7a69c3d3c1c3
Christopher Stone <chstone@microsoft.com>
2019-11-12 17:01:24 -0500

    Fix parameter name

--- sdk/cognitiveservices/FormRecognizer/src/Customizations/Models/AnalyzeUrlRequest.cs
+++ sdk/cognitiveservices/FormRecognizer/src/Customizations/Models/AnalyzeUrlRequest.cs
@@ -1,11 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 
 using System;
+using Newtonsoft.Json;
 
 namespace Microsoft.Azure.CognitiveServices.FormRecognizer.Models
 {
     public class AnalyzeUrlRequest
     {
+        [JsonProperty(PropertyName = "source")]
         public Uri Source { get; set; }
     }
 }
