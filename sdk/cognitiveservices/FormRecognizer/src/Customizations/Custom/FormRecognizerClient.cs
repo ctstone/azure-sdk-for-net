f3683af4dcb3bb79122f7f9abeda6913d2393be5
Christopher Stone <chstone@microsoft.com>
2019-11-15 11:32:20 -0500

    update for swagger change

--- sdk/cognitiveservices/FormRecognizer/src/Customizations/Custom/FormRecognizerClient.cs
+++ sdk/cognitiveservices/FormRecognizer/src/Customizations/Custom/FormRecognizerClient.cs
@@ -523,7 +523,7 @@ namespace Microsoft.Azure.CognitiveServices.FormRecognizer
             HttpStatusCode _statusCode = _httpResponse.StatusCode;
             cancellationToken.ThrowIfCancellationRequested();
             string _responseContent = null;
-            if ((int)_statusCode != 200)
+            if ((int)_statusCode != 204)
             {
                 var ex = new ErrorResponseException(string.Format("Operation returned an invalid status code '{0}'", _statusCode));
                 try
@@ -608,7 +608,7 @@ namespace Microsoft.Azure.CognitiveServices.FormRecognizer
             {
                 queryParameters = new List<(string, string)>
                 {
-                    ("includeTextDetails", includeTextDetails.Value.ToString()),
+                    ("includeTextDetails", includeTextDetails.Value.ToString().ToLowerInvariant()),
                 };
             }
             return AnalyzeWithHttpMessagesAsync<AnalyzeWithCustomModelHeaders>(
@@ -675,7 +675,7 @@ namespace Microsoft.Azure.CognitiveServices.FormRecognizer
             {
                 queryParameters = new List<(string, string)>
                 {
-                    ("includeTextDetails", includeTextDetails.Value.ToString()),
+                    ("includeTextDetails", includeTextDetails.Value.ToString().ToLowerInvariant()),
                 };
             }
             return AnalyzeWithHttpMessagesAsync<AnalyzeWithCustomModelHeaders>(
@@ -688,73 +688,6 @@ namespace Microsoft.Azure.CognitiveServices.FormRecognizer
                 null,
                 fileStream,
                 null,
-                contentType.ToString(),
-                queryParameters,
-                customHeaders,
-                cancellationToken);
-        }
-
-        /// <summary>
-        /// Analyze Form from byte array
-        /// </summary>
-        /// <remarks>
-        /// Extract key-value pairs, tables, and semantic values from a given document.
-        /// The input document must be of one of the supported content types -
-        /// 'application/pdf', 'image/jpeg', 'image/png' or 'image/tiff'.
-        /// Alternatively, use 'application/json' type to specify the location (Uri or
-        /// local path) of the document to be analyzed.
-        /// </remarks>
-        /// <param name='modelId'>
-        /// Model identifier.
-        /// </param>
-        /// <param name='includeTextDetails'>
-        /// Include text lines and element references in the result.
-        /// </param>
-        /// <param name='byteArray'>
-        /// .json, .pdf, .jpg, .png or .tiff type byte array.
-        /// </param>
-        /// <param name='contentType'>
-        /// Content type of the bytes.
-        /// </param>
-        /// <param name='customHeaders'>
-        /// Headers that will be added to request.
-        /// </param>
-        /// <param name='cancellationToken'>
-        /// The cancellation token.
-        /// </param>
-        /// <exception cref="ErrorResponseException">
-        /// Thrown when the operation returned an invalid status code
-        /// </exception>
-        /// <exception cref="ValidationException">
-        /// Thrown when a required parameter is null
-        /// </exception>
-        /// <exception cref="System.ArgumentNullException">
-        /// Thrown when a required parameter is null
-        /// </exception>
-        /// <return>
-        /// A response object containing the response body and response headers.
-        /// </return>
-        public Task<HttpOperationHeaderResponse<AnalyzeWithCustomModelHeaders>> AnalyzeWithCustomModelWithHttpMessagesAsync(Guid modelId, byte[] byteArray, AnalysisContentType contentType, bool? includeTextDetails = false, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            List<(string, string)> queryParameters = null;
-
-            if (includeTextDetails.HasValue)
-            {
-                queryParameters = new List<(string, string)>
-                {
-                    ("includeTextDetails", includeTextDetails.Value.ToString()),
-                };
-            }
-            return AnalyzeWithHttpMessagesAsync<AnalyzeWithCustomModelHeaders>(
-                Trace_AnalyzeCustom,
-                "custom/models/{modelId}/analyze",
-                new Dictionary<string, string>
-                {
-                    { "modelId", modelId.ToString() },
-                },
-                null,
-                null,
-                byteArray,
                 contentType.ToString(),
                 queryParameters,
                 customHeaders,
