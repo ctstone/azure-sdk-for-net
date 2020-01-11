30fd67a280f332856c3b1eacbf845e2b432af7d8
Christopher Stone <chstone@microsoft.com>
2020-01-11 08:38:56 -0500

    cleanup

--- sdk/cognitiveservices/FormRecognizer/tests/UnitTest1.cs
+++ sdk/cognitiveservices/FormRecognizer/tests/UnitTest1.cs
@@ -1,7 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System;
 using Xunit;
 
-namespace Azure.AI.FormRecognizer.Test
+namespace Azure.AI.FormRecognizer.Tests
 {
     public class UnitTest1
     {
