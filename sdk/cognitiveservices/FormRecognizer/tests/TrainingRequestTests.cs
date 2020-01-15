e9b9e3dd196275cc95d01b79eb7a366c7c906661
Christopher Stone <chstone@microsoft.com>
2020-01-15 10:13:28 -0500

    add train request tests

---
+++ sdk/cognitiveservices/FormRecognizer/tests/TrainingRequestTests.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Text;
+using Azure.Core;
+using Xunit;
+using static Azure.AI.FormRecognizer.FormRecognizerClientOptions;
+
+namespace Azure.AI.FormRecognizer.Tests
+{
+    public class TrainingRequestTests
+    {
+        [Fact]
+        public void Request_SourceFilter_IsOptional()
+        {
+            var request = new TrainingRequest { SourceFilter = null };
+        }
+
+        [Fact]
+        public void Request_UseLabelFile_IsOptional()
+        {
+            var request = new TrainingRequest { UseLabelFile = null };
+        }
+    }
+}
