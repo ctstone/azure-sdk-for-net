8fd22de68528c5cc5c9885e4103fb80bcf295669
Christopher Stone <chstone@microsoft.com>
2020-01-16 13:18:31 -0500

    update pipeline extension tests

--- sdk/cognitiveservices/FormRecognizer/tests/Extensions/PipelineExtensionTests.cs
+++ sdk/cognitiveservices/FormRecognizer/tests/Extensions/PipelineExtensionTests.cs
@@ -25,11 +25,96 @@ namespace Azure.AI.FormRecognizer.Tests.Extensions
 
         private readonly MockTransport _mockTransport;
         private readonly HttpPipeline _pipeline;
+        private readonly FormRecognizerClientOptions _options;
 
         public PipelineExtensionTests()
         {
             _mockTransport = new MockTransport();
             _pipeline = new HttpPipeline(_mockTransport);
+            _options = new FormRecognizerClientOptions();
+        }
+
+        [Fact]
+        public void CreateTrainRequest_CreatesRequest()
+        {
+            // Arrange
+            var trainRequest = new TrainingRequest { Source = "http://localhost" };
+
+            // Act
+            var request = _pipeline.CreateTrainRequest(trainRequest, _options);
+
+            // Assert
+            long requestSize;
+            Assert.Equal(RequestMethod.Post, request.Method);
+            Assert.Equal($"/custom/models", request.Uri.PathAndQuery);
+            Assert.True(request.Content.TryComputeLength(out requestSize));
+            Assert.True(requestSize > 0);
+            var requestContentType = request.Headers.FirstOrDefault((x) => x.Name == "Content-Type");
+            Assert.Equal("application/json", requestContentType.Value);
+        }
+
+        [Fact]
+        public void CreateGetModelRequest_CreatesRequest()
+        {
+            // Arrange
+            var modelId = "fake-model-id";
+
+            // Act
+            var request = _pipeline.CreateGetModelRequest(modelId);
+
+            // Assert
+            Assert.Equal(RequestMethod.Get, request.Method);
+            Assert.Equal($"/custom/models/{modelId}", request.Uri.PathAndQuery);
+            Assert.Null(request.Content);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("summary")]
+        [InlineData("full")]
+        public void CreateListModelsRequest_CreatesRequest(string op)
+        {
+            // Arrange
+            var expectQuery = op == default ? String.Empty : $"?op={op}";
+
+            // Act
+            var request = _pipeline.CreateListModelsRequest(op: op);
+
+            // Assert
+            Assert.Equal(RequestMethod.Get, request.Method);
+            Assert.Equal($"/custom/models{expectQuery}", request.Uri.PathAndQuery);
+            Assert.Null(request.Content);
+        }
+
+        [Fact]
+        public void CreateDeleteModelRequest_CreatesRequest()
+        {
+            // Arrange
+            var modelId = "fake-model-id";
+
+            // Act
+            var request = _pipeline.CreateDeleteModelRequest(modelId);
+
+            // Assert
+            Assert.Equal(RequestMethod.Delete, request.Method);
+            Assert.Equal($"/custom/models/{modelId}", request.Uri.PathAndQuery);
+            Assert.Null(request.Content);
+        }
+
+        [Fact]
+        public void CreateGetAnalysisRequest_CreatesRequest()
+        {
+            // Arrange
+            var basePath = "/fake/base/path";
+            var operationId = "fake-id";
+
+            // Act
+            var request = _pipeline.CreateGetAnalysisRequest(basePath, operationId);
+
+            // Assert
+            Assert.Equal(RequestMethod.Get, request.Method);
+            Assert.Equal($"{basePath}/analyzeResults/{operationId}", request.Uri.PathAndQuery);
+            Assert.Null(request.Content);
         }
 
         [Fact]
@@ -41,18 +126,38 @@ namespace Azure.AI.FormRecognizer.Tests.Extensions
             var contentType = FormContentType.Pdf;
             var options = new MockOptions { Foo = "foo" };
 
-
             // Act
             var request = _pipeline.CreateAnalyzeStreamRequest<MockOptions>(basePath, stream, contentType, options, SetOption);
+
+            // Assert
+            long requestSize;
+            Assert.Equal(RequestMethod.Post, request.Method);
+            Assert.Equal($"{basePath}/analyze?fake-option=foo", request.Uri.PathAndQuery);
+            Assert.True(request.Content.TryComputeLength(out requestSize));
+            Assert.Equal(stream.Length, requestSize);
+            var requestContentType = request.Headers.FirstOrDefault((x) => x.Name == "Content-Type");
+            Assert.Equal("application/pdf", requestContentType.Value);
+        }
+
+        [Fact]
+        public void CreateAnalyzeUriRequest_CreatesRequest()
+        {
+            // Arrange
+            var basePath = "/fake/base/path";
+            var uri = new Uri("http://localhost/fake-file.pdf");
+            var options = new MockOptions { Foo = "foo" };
+
+            // Act
+            var request = _pipeline.CreateAnalyzeUriRequest<MockOptions>(basePath, uri, _options, options, SetOption);
 
             // Assert
             Assert.Equal(RequestMethod.Post, request.Method);
             Assert.Equal($"{basePath}/analyze?fake-option=foo", request.Uri.PathAndQuery);
             long requestSize;
             Assert.True(request.Content.TryComputeLength(out requestSize));
-            Assert.Equal(stream.Length, requestSize);
+            Assert.True(requestSize > 0);
             var requestContentType = request.Headers.FirstOrDefault((x) => x.Name == "Content-Type");
-            Assert.Equal("application/pdf", requestContentType.Value);
+            Assert.Equal("application/json", requestContentType.Value);
         }
 
         private static void SetOption(MockOptions options, Request request)
