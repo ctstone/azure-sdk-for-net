f2b67b14cf26a5d583f3af02dab3e5053af83980
Christopher Stone <chstone@microsoft.com>
2020-01-16 11:15:32 -0500

    add prebuilt client tests

---
+++ sdk/cognitiveservices/FormRecognizer/tests/Core/PrebuiltFormClientTests.cs
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.AI.FormRecognizer.Core;
+using Azure.AI.FormRecognizer.Models;
+using Azure.AI.FormRecognizer.Tests.TestUtilities;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Core.Testing;
+using Xunit;
+
+namespace Azure.AI.FormRecognizer.Tests.Core
+{
+    public class PrebuiltFormClientTests
+    {
+        [Fact]
+        public void Constructor_Creates_ReceiptClient()
+        {
+            // Act
+            var client = GetClient();
+
+            // Assert
+            Assert.NotNull(client.Receipt);
+        }
+
+        [Fact]
+        public void Class_Is_AnalyzeClient()
+        {
+            // Act
+            var client = GetClient();
+
+            // Assert
+            Assert.True(client is AnalyzeClient<AnalyzeOptions>);
+        }
+
+        private PrebuiltFormClient GetClient(params MockResponse[] responses)
+        {
+            var mockTransport = new MockTransport(responses);
+            var pipeline = new HttpPipeline(mockTransport);
+            var options = new FormRecognizerClientOptions();
+            return new PrebuiltFormClient(pipeline, options);
+        }
+    }
+}
