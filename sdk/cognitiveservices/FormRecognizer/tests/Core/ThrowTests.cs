a8839998b45af14b672b67324f4e127d5c12d9ca
Christopher Stone <chstone@microsoft.com>
2020-01-16 11:46:29 -0500

    add throw tests

---
+++ sdk/cognitiveservices/FormRecognizer/tests/Core/ThrowTests.cs
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Azure.AI.FormRecognizer.Core;
+using Azure.AI.FormRecognizer.Models;
+using Azure.AI.FormRecognizer.Tests.TestUtilities;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Core.Testing;
+using Xunit;
+
+namespace Azure.AI.FormRecognizer.Tests.Core
+{
+    public class ThrowTests
+    {
+        [Fact]
+        public void IfMissing_Throws_WhenNull()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => Throw.IfMissing(null as string, "foo"));
+            Assert.Equal("foo", ex.ParamName);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void IfNullOrEmpty_Throws_WhenNullOrEmpty(string value)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Throw.IfNullOrEmpty(value, "foo"));
+            Assert.Equal("foo", ex.ParamName);
+        }
+
+        [Theory]
+        [InlineData("http://localhost", true)]
+        [InlineData("http://localhost/path", true)]
+        [InlineData("http://localhost:1234/path", true)]
+        [InlineData("https://localhost:1234/path", true)]
+        [InlineData("//localhost", false)]
+        [InlineData("localhost", false)]
+        [InlineData("file:///foo.txt", false)]
+        public void IfInvalidUri_Throws_ForInvalidUri(string uriText, bool isValid)
+        {
+            var uri = new Uri(uriText, UriKind.RelativeOrAbsolute);
+            if (isValid)
+            {
+                Throw.IfInvalidUri(uri, "foo");
+            }
+            else
+            {
+                var ex = Assert.Throws<ArgumentException>(() => Throw.IfInvalidUri(uri, "foo"));
+                Assert.Equal("foo", ex.ParamName);
+            }
+        }
+    }
+}
