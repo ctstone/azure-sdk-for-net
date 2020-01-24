dac827138f5263ac33a8ac5b24aa14a0729d896d
Chris Stone <chstone@microsoft.com>
2020-01-24 07:06:22 -0800

    add overload for endpoint/key

--- sdk/formrecognizer/src/FormRecognizerClient.cs
+++ sdk/formrecognizer/src/FormRecognizerClient.cs
@@ -42,6 +42,39 @@ namespace Azure.AI.FormRecognizer
         /// <summary>
         /// Initializes a new instance of the <see cref="FormRecognizerClient"/> class.
         /// </summary>
+        /// <param name="endpoint">
+        /// The assigned endpoint for your subscriptionKey. E.g. for `eastus`:
+        ///
+        /// ```csharp
+        /// var endpoint = CognitiveEndpoint.EastUnitedStates;
+        /// ```
+        /// </param>
+        /// <param name="subscriptionKey">Your assigned subscription key, copied from https://portal.azure.com/</param>
+        public FormRecognizerClient(CognitiveEndpoint endpoint, string subscriptionKey)
+            : this(new CognitiveCredential(endpoint, subscriptionKey))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormRecognizerClient"/> class.
+        /// </summary>
+        /// <param name="endpoint">
+        /// The assigned endpoint for your subscriptionKey. E.g. for `eastus`:
+        ///
+        /// ```csharp
+        /// var endpoint = CognitiveEndpoint.EastUnitedStates;
+        /// ```
+        /// </param>
+        /// <param name="subscriptionKey">Your assigned subscription key, copied from https://portal.azure.com/</param>
+        /// <param name="options">Optional service parameters.</param>
+        public FormRecognizerClient(CognitiveEndpoint endpoint, string subscriptionKey, FormRecognizerClientOptions options)
+            : this(new CognitiveCredential(endpoint, subscriptionKey), options)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormRecognizerClient"/> class.
+        /// </summary>
         /// <param name="credential">A Cognitive Services credential object.</param>
         public FormRecognizerClient(CognitiveCredential credential)
         : this(credential, new FormRecognizerClientOptions())
