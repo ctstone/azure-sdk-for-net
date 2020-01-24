6f9575596357956bacb25e5d0190f1237bf0db01
Chris Stone <chstone@microsoft.com>
2020-01-23 20:46:27 -0800

    convert FormContentType to readonly struct

--- sdk/formrecognizer/src/FormContentType.cs
+++ sdk/formrecognizer/src/FormContentType.cs
@@ -1,23 +1,79 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.ComponentModel;
+
 namespace Azure.AI.FormRecognizer
 {
     /// <summary>
     /// Form content type for local files.
     /// </summary>
-    public enum FormContentType
+    public readonly struct FormContentType : IEquatable<FormContentType>
     {
-        /// <summary>application/pdf</summary>
-        Pdf = 1,
+        private readonly string _value;
 
-        /// <summary>image/png</summary>
-        Png = 2,
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormContentType"/> struct.
+        /// </summary>
+        /// <param name="value">MIME type of form document.</param>
+        public FormContentType(string value)
+        {
+            _value = value ?? throw new ArgumentNullException(nameof(value));
+        }
 
-        /// <summary>image/jpeg</summary>
-        Jpeg = 3,
+        /// <summary>
+        /// Pdf.
+        /// </summary>
+        public static FormContentType Pdf => "application/pdf";
 
-        /// <summary>image/tiff</summary>
-        Tiff = 4,
+        /// <summary>
+        /// Png.
+        /// </summary>
+        public static FormContentType Png => "image/png";
+
+        /// <summary>
+        /// Jpeg.
+        /// </summary>
+        public static FormContentType Jpeg => "image/jpeg";
+
+        /// <summary>
+        /// Tiff.
+        /// </summary>
+        public static FormContentType Tiff => "image/tiff";
+
+        /// <inheritdoc />
+        public bool Equals(FormContentType other) => string.Equals(_value, other._value, StringComparison.Ordinal);
+
+        /// <inheritdoc/>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj) => obj is FormContentType other && Equals(other);
+
+        /// <inheritdoc/>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode() => _value?.GetHashCode() ?? 0;
+
+        /// <inheritdoc/>
+        public override string ToString() => _value;
+
+        /// <summary>
+        /// Converts a string to a <see cref="FormContentType"/>.
+        /// </summary>
+        /// <param name="value">The string value to convert.</param>
+        public static implicit operator FormContentType(string value) => new FormContentType(value);
+
+        /// <summary>
+        /// Determines if two <see cref="FormContentType"/> values are the same.
+        /// </summary>
+        /// <param name="left">The first <see cref="FormContentType"/> to compare.</param>
+        /// <param name="right">The first <see cref="FormContentType"/> to compare.</param>
+        public static bool operator ==(FormContentType left, FormContentType right) => left.Equals(right);
+
+        /// <summary>
+        /// Determines if two <see cref="FormContentType"/> values are different.
+        /// </summary>
+        /// <param name="left">The first <see cref="FormContentType"/> to compare.</param>
+        /// <param name="right">The first <see cref="FormContentType"/> to compare.</param>
+        public static bool operator !=(FormContentType left, FormContentType right) => !left.Equals(right);
     }
 }
--- sdk/formrecognizer/src/Http/FormHttpHeader.cs
+++ sdk/formrecognizer/src/Http/FormHttpHeader.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using Azure.Core;
 
 namespace Azure.AI.FormRecognizer.Http
@@ -15,25 +14,9 @@ namespace Azure.AI.FormRecognizer.Http
 
         public static class Common
         {
-            public static readonly HttpHeader PdfContentType
-                = new HttpHeader(HttpHeader.Names.ContentType, "application/pdf");
-            public static readonly HttpHeader PngContentType
-                = new HttpHeader(HttpHeader.Names.ContentType, "image/png");
-            public static readonly HttpHeader JpegContentType
-                = new HttpHeader(HttpHeader.Names.ContentType, "image/jpeg");
-            public static readonly HttpHeader TiffContentType
-                = new HttpHeader(HttpHeader.Names.ContentType, "image/tiff");
-
             public static HttpHeader ForContentType(FormContentType contentType)
             {
-                return contentType switch
-                {
-                    FormContentType.Pdf => PdfContentType,
-                    FormContentType.Png => PngContentType,
-                    FormContentType.Jpeg => JpegContentType,
-                    FormContentType.Tiff => TiffContentType,
-                    _ => throw new NotSupportedException($"Content-Type {contentType} is not supported."),
-                };
+                return new HttpHeader(HttpHeader.Names.ContentType, contentType.ToString());
             }
         }
     }
